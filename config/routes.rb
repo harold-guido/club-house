8ce1b26f9fde24c8570c6ef8e0ad1512536040dc
Harold Parra <gc.harold.parra@gmail.com>
2021-02-10 14:54:46 +0000

    Configurate controllers option for devise model user in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :ratings
   resources :posts
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
