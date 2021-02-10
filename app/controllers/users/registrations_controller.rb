9598dc79ffff623b354fe5f580e9f20477330bb7
Harold Parra <gc.harold.parra@gmail.com>
2021-02-10 15:00:36 +0000

    Configure posts index view to render new partials

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+
+  def acount_update_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
+  end
 
   # GET /resource/sign_up
   # def new
